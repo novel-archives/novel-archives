3bf68640012092f1b3fba316189cc7e4dd661f02
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-01-24 18:38:45 +0900

    Revert "Merge pull request #44 from novel-archives/feature/xid"
    This reverts commit 79b19d078b33118349ae2f5d4aa4b23495a428f2, reversing
    changes made to a40d0c9144b479109579df90b1a3b3000f818a49.

--- kernel/backend/src/drivers/repositories/id_generator.rs
+++ kernel/backend/src/drivers/repositories/id_generator.rs
@@ -1,13 +1,29 @@
 use crate::prelude::*;
-pub struct IdGenerator {}
+use harsh::Harsh;
+use std::sync::Mutex;
+pub struct IdGenerator {
+    harsh: Harsh,
+    prefix_key: Mutex<u64>,
+}
 
 impl IdGenerator {
-    #[allow(clippy::new_without_default)]
-    pub fn new() -> Self {
-        Self {}
+    const PROJECT_EPOCH: i64 = 1610722800000000000;
+    pub fn new<T: Into<Vec<u8>>>(salt: T) -> IdGenerator {
+        IdGenerator {
+            harsh: Harsh::builder().salt(salt).build().unwrap(),
+            prefix_key: Mutex::new(rand::random::<u32>() as u64),
+        }
     }
     pub fn generate<T>(&self) -> Result<Id<T>, Error> {
-        Ok(Id::try_new(xid::new().to_string()).unwrap())
+        let elapsed = times::now().timestamp_nanos() - Self::PROJECT_EPOCH;
+        let mut prefix_key_mutex = self.prefix_key.lock().unwrap();
+        let prefix_key = *prefix_key_mutex;
+        if prefix_key == u64::MAX {
+            *prefix_key_mutex = 0;
+        }
+        *prefix_key_mutex += 1;
+        let h = self.harsh.encode(&[prefix_key, elapsed as u64]);
+        Ok(Id::try_new(h).unwrap())
     }
 }
 
@@ -21,8 +37,16 @@ mod tests {
     struct IdTag;
     #[test]
     fn generate_works() {
-        let id_gen = IdGenerator::new();
+        let id_gen = IdGenerator::new("salt");
         let id = id_gen.generate::<IdTag>().unwrap();
         assert_ne!(id.raw_id(), "");
+    }
+
+    #[test]
+    fn prefix_key_works() {
+        let id_gen = IdGenerator::new("salt");
+        let before = *id_gen.prefix_key.lock().unwrap();
+        let _ = id_gen.generate::<IdTag>();
+        assert_eq!(before + 1, *id_gen.prefix_key.lock().unwrap());
     }
 }
